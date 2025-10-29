1a18a41d35a76e755001daec1e32ba6773d88789
蔡鳳駿 <eoleedimin@gmail.com>
2025-10-29 16:01:41 +0800

    fix: make checkpoint a string

--- test_so762.sh
+++ test_so762.sh
@@ -1,6 +1,6 @@
 python test.py \
     --dataset mispeech/speechocean762 \
     --model ClusterScorer \
-    --checkpoint exp/SSLfeat_fluency+prosodicScore/1e-3-3-25-32-ClusterScorer-br/0/models/best_audio_model.pth \
+    --checkpoint "exp/SSLfeat_fluency+prosodicScore/1e-3-3-25-32-ClusterScorer-br/0/models/best_audio_model.pth" \
     --kmeans_model exp/kmeans/kmeans_model.joblib \
     --aspect fluency prosodic
